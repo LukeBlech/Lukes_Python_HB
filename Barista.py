0c6f7686ca44c5d3c06098f47a70a66512791f74
Luke <83956998+LukeBlech@users.noreply.github.com>
2022-04-12 09:05:30 +0200

    updates to the war

---
+++ Barista.py
@@ -0,0 +1,15 @@
+#coffee = [2,10,5,3,9]) #'Sorry, but the smallest waiting time possible is: 85')
+#coffee = ([4,3,2]) #'Sorry, but the smallest waiting time possible is: 22')
+#coffee = [20,5]) #'Sorry, but the smallest waiting time possible is: 32')
+#coffee = [20,5,4,3,1,5,7,8]) #'Sorry, but the smallest waiting time possible is: 211')
+#coffee = [5,4,3,2,1] #'Sorry, but the smallest waiting time possible is: 55')
+
+
+
+
+def barista(coffees):
+    c = 0
+    for i in coffees:
+        if i != 0:
+            c = c.append(1) + 2
+    return 'Solution:'
--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,21 +1,23 @@
 def rps(p1, p2):
-            if p1 == 'scissors':
-                p1 = 1
-            if p1 == 'paper':
-                p1 = 2
-            if p1 == 'rock':
-                p1 == 3
-            if p2 == 'scissors':
-                p2 = 1
-            if p2 == 'paper':
-                p2 = 2
-            if p2 == 'rock':
-                p2 == 3
-        # 1<2<3>1
-            if p1 - p2 < 0:
-                return "Player 1 won!"
-            if p1 - p2 == [0]:
-                return "Draw!"
-            else:
-                return "Player 2 won!"
-            
+    for i in p1:
+        if i in p1 == 'scissors':
+            p1 = int(1)
+        if p1 == 'paper':
+            p1 = int(2)
+        if p1 == 'rock':
+            p1 == int(3)
+    for i in p2:
+        if p2 == 'scissors':
+            p2 = int(1)
+        if p2 == 'paper':
+            p2 = int(2)
+        if p2 == 'rock':
+            p2 == int(3)
+    # 1<2<3>1
+    if p1 - p2 < 0:
+        return "Player 1 won!"
+    if p1 - p2 == [0]:
+        return "Draw!"
+    else:
+        return "Player 2 won!"
+
